564e68c58aeca923547c835fd1a6aa9e7ad6120b
Renesanse <dsrenesanse@gmail.com>
2019-01-21 20:28:04 +0300

    stop copy and paste keys

--- lib/src/resources/keys.dart
+++ lib/src/resources/keys.dart
@@ -1,3 +1,3 @@
-final apikeys = ["77f751d4acf449db8526f0e97e1e50b3",
+final apikeys = ["2fdf204df2ed41a58309733182d9e774",
                   "ead93fedf9574cd0ab03790d1817a1f6",
                   "4a75fe36e37b4cdbab71fd0346ca423f"];
