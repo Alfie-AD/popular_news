483f6591c42c2a7dab36df4c38b9355da79d6bf6
Renesanse <dsrenesanse@gmail.com>
2019-01-30 22:03:48 +0300

    LangAuto update

--- lib/src/blocs/news_bloc.dart
+++ lib/src/blocs/news_bloc.dart
@@ -8,7 +8,13 @@ class NewsBloc {
   get news => _controllerNews.stream;
   get saved => _controllerSaved.stream;
 
-  fetchAllNews() async => _controllerNews.add(await _repository.getNews());
+  fetchAllNews() async {
+    _controllerNews.add(await _repository.getNews());
+    Stream.periodic(Duration(hours: 1)).listen((onData) async {
+      mainArticles.clear();
+      _controllerNews.add(await _repository.getNews());
+    });
+  }
 
   fetchSavedNews() async => _controllerSaved.add(await _repository.getSavedNews());
 
